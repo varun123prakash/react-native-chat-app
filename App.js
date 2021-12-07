645ee3f90c3c78bdf469a0d264289f59b3f99f33
varun prakash <varun.prakash@dianapps.com>
2021-12-07 17:23:15 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+const App = ()=>{
+  return(
+    <View style={{flex:1,justifyContent:'center',alignItems:'center'}}>
+      <Text>hello</Text>
+      
+    </View>
+  )
+}
+
+export default App;
